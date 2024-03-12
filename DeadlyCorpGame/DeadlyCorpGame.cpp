14247a589e89d18fe3f1da21a10ef65f92f08ef9
Strtk001 Travis Strawbridge <Strtk001@mymail.unisa.edu.au>
2024-03-12 13:46:45 +1030

    initial commit

---
+++ DeadlyCorpGame/DeadlyCorpGame.cpp
@@ -0,0 +1,12 @@
+﻿// DeadlyCorpGame.cpp : Defines the entry point for the application.
+//
+
+#include "DeadlyCorpGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
